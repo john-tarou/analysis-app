d06c35d84f2e7afd2afb6657c93493164f59d00d
john-tarou <keiko06.mo@gmail.com>
2022-08-11 21:24:35 +0900

    診断投稿機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :career_anchor
+ 
   
   validates :name, presence: true
   validates :company_name, presence: true
