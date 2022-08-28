c50ba05ead9346476a589c590cc7f75f99c20f1d
john-tarou <keiko06.mo@gmail.com>
2022-08-28 15:57:24 +0900

    管理者カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_033711) do
+ActiveRecord::Schema.define(version: 2022_08_28_065512) do
 
   create_table "career_anchors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "question_1", null: false
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2022_08_11_033711) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
