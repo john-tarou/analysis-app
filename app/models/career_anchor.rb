b856f675c9d1912cf701f586f69a768add428898
john-tarou <keiko06.mo@gmail.com>
2022-09-08 22:15:18 +0900

    career_anchorモデルの修正

--- app/models/career_anchor.rb
+++ app/models/career_anchor.rb
@@ -3,46 +3,46 @@ class CareerAnchor < ApplicationRecord
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  has_many :one_to_fours
+  belongs_to :one_to_fours
 
-  validates :question_1, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_2, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_3, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_4, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_5, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_6, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_7, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_8, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_9, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_10, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_11, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_12, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_13, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_14, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_15, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_16, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_17, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_18, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_19, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_20, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_21, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_22, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_23, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_24, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_25, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_26, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_27, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_28, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_29, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_30, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_31, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_32, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_33, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_34, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_35, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_36, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_37, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_38, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_39, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  validates :question_40, presence: true,numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
+  validates :question_1, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_2, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_3, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_4, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_5, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_6, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_7, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_8, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_9, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_10, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_11, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_12, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_13, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_14, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_15, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_16, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_17, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_18, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_19, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_20, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_21, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_22, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_23, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_24, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_25, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_26, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_27, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_28, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_29, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_30, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_31, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_32, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_33, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_34, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_35, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_36, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_37, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_38, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_39, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
+  validates :question_40, presence: true,numericality: { other_than: 0, message: "can't be blank" } 
 end
